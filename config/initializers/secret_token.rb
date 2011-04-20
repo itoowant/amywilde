9e6649a6b219c670b7214106c7a2b751e0963069
Zain Raza <zain@mobdub.com>
2011-04-20 04:41:49 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Amywilde::Application.config.secret_token = 'd92a4308fa36d4b6c272541c7fe94352a7ab228157cdd3213c6e4c7a1cc0263be38a124f5ab65478a21e26d62ac4bcba20ab906f2e4f00d1c84d4db3684ea720'
